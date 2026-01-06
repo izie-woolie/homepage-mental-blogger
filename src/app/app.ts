6c1dc93b27c85407126e4ebf1104d2a86ffd1338
Izie Zahba <dushara.fahzad62@gmail.com>
2026-01-06 15:01:58 +0800

    Woolie is Live

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('homepage-mental-blogger');
+
+  resultString: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  fetchString() {
+    this.http.get('https://mental-blogger.onrender.com', { responseType: 'text' }).subscribe({
+      next: (data) => {
+        this.resultString = data;
+        console.log(data);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.fetchString();
+  }
 }
